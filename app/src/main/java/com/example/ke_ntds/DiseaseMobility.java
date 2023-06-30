0fde46aa2ccc98647891f676dbe998f4cc5112bc
SSifa <stivengombo@gmail.com>
2023-06-30 13:33:01 +0300

    Project updates

--- app/src/main/java/com/example/ke_ntds/DiseaseMobility.java
+++ app/src/main/java/com/example/ke_ntds/DiseaseMobility.java
@@ -1,14 +1,34 @@
 package com.example.ke_ntds;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class DiseaseMobility extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease_mobility);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new CasesFragment(), "CASES");
+        vpAdapter.addFragment(new ReportFragment(), "REPORT CASE");
+        viewPager.setAdapter(vpAdapter);
     }
 }
